501e1ba8cecf261610cd405fef1218c3f7382962
Edward Smit <edwardsmit@xs4all.nl>
2020-10-18 21:19:00 +0200

    New ING format

--- lib/ing2ynab.ex
+++ lib/ing2ynab.ex
@@ -3,7 +3,8 @@ defmodule Ing2ynab do
   A simple converter from ING Bank CSV's to YNAB CSV's
   """
 
-  alias NimbleCSV.RFC4180, as: CSV
+  NimbleCSV.define(CSV_in, separator: ";")
+  alias NimbleCSV.RFC4180, as: CSV_out
 
   defp header() do
     ~w(Date Payee Memo Amount)
@@ -15,19 +16,18 @@ defmodule Ing2ynab do
     run(opts)
   end
 
-  defp run([:file file])
+  defp run(file: file) do
     file
     |> File.stream!()
-    |> CSV.parse_stream()
+    |> CSV_in.parse_stream()
     |> Stream.map(&convert_ing_line/1)
     |> Stream.transform(:header, fn row, location ->
       if location == :header, do: {[header(), row], :row}, else: {[row], location}
     end)
-    |> CSV.dump_to_stream()
+    |> CSV_out.dump_to_stream()
     |> Stream.into(File.stream!("ynab.csv"))
     |> Stream.run()
   end
-
 
   defp convert_ing_line([
          date,
@@ -38,7 +38,9 @@ defmodule Ing2ynab do
          debit_credit,
          amount,
          transaction_type,
-         notifications
+         notifications,
+         _new_balance,
+         _tag
        ]) do
     [
       map_date(date),
@@ -51,15 +53,15 @@ defmodule Ing2ynab do
   defp map_date(date) do
     date
     |> Timex.parse!("{YYYY}{0M}{0D}")
-    |> Timex.format!("{0M}/{0D}/{YYYY}")
+    |> Timex.format!("{0D}/{0M}/{YYYY}")
   end
 
-  defp map_amount("Debit", amount) do
+  defp map_amount("Af", amount) do
     "-#{amount}"
     |> String.replace(",", ".")
   end
 
-  defp map_amount("Credit", amount) do
+  defp map_amount("Bij", amount) do
     amount
     |> String.replace(",", ".")
   end
